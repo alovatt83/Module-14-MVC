c5e49c506b5fc2180a5e64b1ce01abf838eab374
Allen <allenlovatt@gmail.com>
2021-03-14 20:25:13 -0400

    rrors

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'us-cdbr-east-03.cleardb.com',
+      host: 'us-cdbr-east-03.cleardb.comg',
       dialect: 'mysql',
       port: 3306
     });
